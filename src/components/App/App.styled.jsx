e830033bfbf5b4396b8384e2acec531846a0af1b
Vitalii Smyrnov <113116677+Vetal-Smyrnov@users.noreply.github.com>
2023-04-17 14:32:46 +0300

    222

--- src/components/App/App.styled.jsx
+++
@@ -1,7 +0,0 @@
-import styled from '@emotion/styled';
-
-export const Container = styled.div`
-  height: '100vh';
-  font-size: 40px;
-  color: #010101;
-`;
--- src/components/Notification/Notification.styled.jsx
+++
@@ -1,5 +0,0 @@
-import styled from '@emotion/styled';
-
-export const Message = styled.p`
-  text-align: center;
-`;
