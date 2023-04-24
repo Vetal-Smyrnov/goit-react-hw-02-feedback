512aa0a6861b152c069182daa84396499379c746
Vitalii Smyrnov <113116677+Vetal-Smyrnov@users.noreply.github.com>
2023-04-24 16:26:51 +0300

    init 5

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom/client';
+import 'modern-normalize';
+import { Global, ThemeProvider } from '@emotion/react';
+import { App } from 'components';
+import { GlobalStyles, theme } from 'styles';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <Global styles={GlobalStyles} />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
